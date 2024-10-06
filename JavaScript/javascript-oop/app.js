caa45fdeb47af86426e2121974abe68b336e2a5d
kittinan4452 <kittinan4452@gmail.com>
2024-10-06 11:06:02 +0700

    DAY3: -  javascritp-basic -  javascritpES6 -  javascritpOOP -  Course completed

--- JavaScript/javascript-oop/app.js
+++ JavaScript/javascript-oop/app.js
@@ -1,7 +1,11 @@
 //สร้าง class
 class User{
     // property
-    #name
+
+    //protected
+    _name
+
+    //  private 
     #password
 
     static database=[]
@@ -9,34 +13,54 @@ class User{
 
     //parameter constructor
     constructor(name,password){
-        this.#name=name
+        this._name=name
         this.#password=password
        
     }
     // method
     showDetail(){
-        console.log("ชื่อผู้ใช่"+this.#name+"รหัส"+this.#password);
+        console.log("ชื่อผู้ใช่"+this._name+"รหัส"+this.#password);
     }
     // setter
     set Name(newName){
-        this.#name = newName
+        this._name = newName
     }
     //getter
     get Name(){
-        return this.#name
+        return this._name
     }
     static addUser(newUser){
         this.database.push(newUser)
     }
 }
-
+class Teacher extends User{
+    #course
+    constructor(name,password,course){
+        super(name,password)
+        this.#course = course
+    }
+    showDetail(){
+        console.log(`ฉันเป็นครูวิชา${this.#course} ชื่อ ${this._name}`);
+    }
+}
+class Student extends User{
+    #score
+    constructor(name,password,score){
+        super(name,password)
+        this.#score = score
+    }
+}
 //สร้าง object
-const user1 = new User("tii",23)
+const user1 = new Teacher("tii",23,"dev")
+const user2 = new Student("tii",23,100)
 
-//เก็บข้อมูล
-User.addUser(user1)
-//แสดงข้อมูล user 
-User.database.forEach((showuser)=>{
-    showuser.showDetail()
-})
+user1.showDetail()
+user2.showDetail()
+// //เก็บข้อมูล
+// User.addUser(user1)
+// //แสดงข้อมูล user 
+
+// User.database.forEach((showuser)=>{
+//     showuser.showDetail()
+// })
 //const user2 = new User("arm",23)
